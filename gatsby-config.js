3c407bd945c0430cfeb31ebd347ba924925c26e3
Bob Merullo <bmerullo85@gmail.com>
2022-11-01 12:38:40 -0600

    added and configured  contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,9 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   siteMetadata: {
@@ -26,6 +29,14 @@ module.exports = {
             },
           ],
         },
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `v2m63u1ogzpz`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY,
       },
     },
     {
