16a4eb71aaf1ee738276d9f189b981cf810b9d6c
Bob Merullo <bmerullo85@gmail.com>
2022-11-11 16:03:03 -0700

    about text

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,13 +12,9 @@ export default function Home() {
         <section className="about-txt">
           <h1>Hello there,</h1>
           <h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            My name is Bob Merullo, I am a web developer based in Denver,
+            Colorado. This is a collection of my personal projects and freelance
+            work.
           </h3>
           <div className="color-box-container">
             <div className="color-box">
