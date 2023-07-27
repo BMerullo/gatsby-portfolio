f4c20abb183de090c4ab55b3e52f060e7fc45775
Bob Merullo <bmerullo85@gmail.com>
2023-07-27 14:33:09 -0600

    added border to info link / added Bob to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,57 +7,65 @@ import homeImg from "../assets/images/home.jpeg"
 import { Link } from "gatsby"
 
 const Footer = () => {
+  const year = new Date().getFullYear()
   return (
     <footer>
-      <ul>
-        <li className="footer-li">
-          <Link
-            to="/"
-            className="nav-link"
-            activeClassName="active-footer-link"
-          >
-            <img className="footer-img" src={homeImg} alt="house" />
-            home
-          </Link>
-        </li>
-        <li className="footer-li">
-          <Link
-            to="/projects"
-            className="nav-link"
-            activeClassName="active-footer-link"
-          >
-            <img className="footer-img" src={projectImg} alt="folder" />
-            projects
-          </Link>
-        </li>
-        <li className="footer-li">
-          <a className="nav-link" href="mailto: bmerullo85@gmail.com">
-            <img className="footer-img" src={emailImg} alt="email" />
-            email
-          </a>
-        </li>
-      </ul>
-      <ul>
-        <li className="footer-li">
-          <a
-            className="nav-link footer-link-flex"
-            href="https://github.com/BMerullo"
-          >
-            <img className="footer-img" src={githubImg} alt="github" />
-            github
-          </a>
-        </li>
+      <section className="footer-links">
+        <ul>
+          <li className="footer-li">
+            <Link
+              to="/"
+              className="nav-link"
+              activeClassName="active-footer-link"
+            >
+              <img className="footer-img" src={homeImg} alt="house" />
+              home
+            </Link>
+          </li>
+          <li className="footer-li">
+            <Link
+              to="/projects"
+              className="nav-link"
+              activeClassName="active-footer-link"
+            >
+              <img className="footer-img" src={projectImg} alt="folder" />
+              projects
+            </Link>
+          </li>
+          <li className="footer-li">
+            <a className="nav-link" href="mailto: bmerullo85@gmail.com">
+              <img className="footer-img" src={emailImg} alt="email" />
+              email
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li className="footer-li">
+            <a
+              className="nav-link footer-link-flex"
+              href="https://github.com/BMerullo"
+            >
+              <img className="footer-img" src={githubImg} alt="github" />
+              github
+            </a>
+          </li>
 
-        <li className="footer-li">
-          <a
-            className="nav-link"
-            href="https://www.linkedin.com/in/bobmerullo/"
-          >
-            <img className="footer-img" src={linkedinImg} alt="linkedin" />
-            linkedin
-          </a>
-        </li>
-      </ul>
+          <li className="footer-li">
+            <a
+              className="nav-link"
+              href="https://www.linkedin.com/in/bobmerullo/"
+            >
+              <img className="footer-img" src={linkedinImg} alt="linkedin" />
+              linkedin
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <p>
+          <span>©{year} </span>Bob Merullo
+        </p>
+      </section>
     </footer>
   )
 }
